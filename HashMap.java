4ec4358c3e26ab09b3352cba95e567a20cebf300
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-17 04:29:20 +0800

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,1 +1,4 @@
+Map<Integer, Integer> map = new HashMap();
+map.put(x, map.getOrDefault(x, 0)); //map.getOrDefault(x, 0) -> hashmap.get(Object key, V defaultValue)
+
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
