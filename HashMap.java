4b716092a266caf17f1b992340e2db48cba2a4b2
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-17 05:40:01 +0800

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,4 +1,36 @@
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#954 - Array of Doubled Pairs
 Map<Integer, Integer> map = new HashMap();
 map.put(x, map.getOrDefault(x, 0)); //map.getOrDefault(x, 0) -> hashmap.get(Object key, V defaultValue)
 
+public boolean canReorderDoubled(int[] arr) {
+    var intsLeft = new HashMap();
+    for (int i : arr) {
+        intsLeft.put(i, intsLeft.getOrDefault(i, 0) + 1);
+    }
+    for (int i : arr) {
+        if (!checkMatch(intsLeft, i)) {
+            return false;
+        }
+    }
+    return true;
+}
+    
+public boolean checkMatch(Map intsLeft, int n) {
+    if (intsLeft.getOrDefault(n, 0) == 0) {
+        return true;
+    }
+    if (n != 0 && n % 2 == 0 && !checkMatch(intsLeft, n / 2)) {
+        return false;
+    }
+    int nCount = intsLeft.get(n);
+    int matchCount = intsLeft.getOrDefault(n * 2, 0);
+    intsLeft.put(n * 2, matchCount - nCount);
+    intsLeft.put(n, 0);
+    return matchCount >= nCount;
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
