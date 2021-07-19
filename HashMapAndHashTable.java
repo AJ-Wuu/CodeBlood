e35754c6f26647f2c8b8e3024db1c05f7ce508c5
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-19 17:12:47 +0800

    Update HashMapAndHashTable.java

--- HashMapAndHashTable.java
+++ HashMapAndHashTable.java
@@ -114,3 +114,40 @@ public void backtrack(List<String> combos, char[] digits, String s, String[] dic
         backtrack(combos, digits, s + Character.toString(letter), dict);
     }
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#30 - Substring with Concatenation of All Words
+public static ArrayList<Integer> findSubstring(String s, String[] words) {
+    if (s == null || words == null || s.length() == 0 || words.length == 0) {
+		return new ArrayList<>();
+	}
+	HashMap<String, Integer> counts = new HashMap<>();
+	for (String word : words) {
+		counts.put(word, counts.getOrDefault(word, 0) + 1);
+	}
+    
+    ArrayList<Integer> r = new ArrayList<>();
+	int sLen = s.length();
+	int num = words.length;
+	int wordLen = words[0].length();
+    
+    for (int i = 0; i < sLen - num * wordLen + 1; i++) {
+		String sub = s.substring(i, i + num * wordLen);
+		if (isConcat(sub, counts, wordLen)) {
+			r.add(i);
+		}
+	}
+	return r;
+}
+
+private static boolean isConcat(String sub, HashMap<String, Integer> counts, int wordLen) {
+	HashMap<String, Integer> seen = new HashMap<>();
+	for (int i = 0; i < sub.length(); i += wordLen) {
+		String sWord = sub.substring(i, i + wordLen);
+		seen.put(sWord, seen.getOrDefault(sWord, 0) + 1);
+	}
+	return seen.equals(counts);
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
