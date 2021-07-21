6155891065029113b241f3329130c02e4cd0bb9d
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-21 21:49:40 +0800

    Update String.java

--- String.java
+++ String.java
@@ -12,7 +12,7 @@ StringBuilder str3 = new StringBuilder(str1.toString());
 //Return the string representation of the passed argument
 str.valueOf(int i); //all data types
 str.valueOf(char[] cc); //only for char arrays
-valueOf(char[] data, int offset, int count) //representation of a specific subarray of the char array argument
+str.valueOf(char[] data, int offset, int count) //representation of a specific subarray of the char array argument
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
