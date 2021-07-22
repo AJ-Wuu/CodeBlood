9532a5153156926da3a9725b8436c07d3d8756a0
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-22 14:24:19 +0800

    Update String.java

--- String.java
+++ String.java
@@ -17,23 +17,48 @@ str.valueOf(char[] data, int offset, int count); //representation of a specific 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //#76 - Minimum Window Substring
-//Template for substring problems:
+//Template for substring problems (with optimization of using array instead of HashMap):
 //1. Shortest Substring w/o repeating characters************************************************************************
 //Algorithms: 1. Use two pointers: start and end to represent a window.
 //            2. Move end to find a valid window.
 //            3. When a valid window is found, move start to find a smaller window.
-int findSubstring(string s){
+public String minWindow(String s, String t) {
+    int[] map = new int[128];
+    for (char c : t.toCharArray()) {
+        map[c]++;
+    }
+    int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, counter = t.length();
+    while (end < s.length()) {
+      final char c1 = s.charAt(end);
+      if (map[c1] > 0) counter--;
+      map[c1]--;
+      end++;
+      while (counter == 0) {
+        if (minLen > end - start) {
+          minLen = end - start;
+          minStart = start;
+        }
+        final char c2 = s.charAt(start);
+        map[c2]++;
+        if (map[c2] > 0) counter++;
+        start++;
+      }
+    }
+
+    return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+  }
+int findSubstring(String string){
     HashMap<Object, Integer> map = new HashMap<Object, Integer>();
     int counter; // check whether the substring is valid
     int begin = 0, end = 0; //two pointers, one point to head and one to tail
     int length; //the length of substring
-    for () { /* initialize the map here */ }
-    while (end < s.size()) {
-        if (map[s[end++]]-- ?) { /* modify counter here */ }
-        while (/* counter condition */) { 
-            /* update d here if finding minimum*/
+    for () { /* initialize the map */ }
+    while (end < string.length) {
+        if (map[string[end++]]-- ?) { /* modify counter */ }
+        while (/* counter condition to see if valid */) { 
+            /* update length here if finding minimum*/
             //increase begin to make it invalid/valid again
-            if (map[s[begin++]]++ ?) { /*modify counter here*/ }
+            if (map[s[begin++]]++ ?) { /* modify counter */ }
         }  
         /* update d here if finding maximum*/
     }
