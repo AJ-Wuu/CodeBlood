6054a9a5b9fc87eb90e7a1c74c8fa288f8a52760
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-24 00:18:39 +0800

    Update Heap_PriorityQueue.java

--- Heap_PriorityQueue.java
+++ Heap_PriorityQueue.java
@@ -50,3 +50,64 @@ PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer
 //1. Pair up k lists and merge each pair.
 //2. After the first pairing, k lists are merged into k/2 lists with average 2N/k length, then k/4, k/8, and so on.
 //3. Repeat this procedure until we get the final sorted linked list.
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#218 - The Skyline Problem
+public static List<List<Integer>> getSkyline(int[][] buildings) {
+    int[][] buildingPoints = new int[buildings.length * 2][2];
+    //Split the original input with size 1x3 into two vectors of size 1x2: e.g. [2,9,10] -> [2,-10] and [9,10], -10 shows that 2 is the start
+    int i = 0;
+    for (int[] building : buildings) {
+        //Negative height for the start
+        buildingPoints[i][0] = building[0];
+        buildingPoints[i][1] = -building[2];
+        //Positive height for the end
+        buildingPoints[i + 1][0] = building[1];
+        buildingPoints[i + 1][1] = building[2];
+        i += 2;
+    }
+    //Sort from smallest to largest based on first column elements 
+    Arrays.sort(buildingPoints, new Comparator<int[]>() {
+        public int compare(int[] o1, int[] o2) {
+            //If the first elements are identical, use the second elements to compare
+            return o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1]; 
+        }
+    });
+
+    List<List<Integer>> res = new ArrayList<>();
+    TreeMap<Integer, Integer> queue = new TreeMap<>();  //Ascending order by default
+    //Since the height of the building is likely to be repeated, let the key record the height, and the value record the number of times the same height appears
+    queue.put(0, 1);
+    int prevMaxHeight = 0;
+    for (int j = 0; j < buildingPoints.length; j++) {
+        int[] buildingPoint = buildingPoints[j];
+        if (buildingPoint[1] < 0) { //isStart == true
+            queue.compute(-buildingPoint[1], (key, value) -> {
+                if (value != null) {
+                    return value + 1;
+                }
+                return 1;
+            });
+            //OR queue.put(-buildingPoint[1], queue.getOrDefault(-buildingPoint[1], 0) + 1);
+        }
+        else { //isStart == false
+            queue.compute(buildingPoint[1], (key, value) -> {
+                if (value == 1) {
+                    return null;
+                }
+                return value - 1;
+            });
+        }
+
+        int currMaxHeight = queue.lastKey();
+        //Since queue is in ascending order by default,
+        //if height changes from previous height, then this building x becomes critical x,
+        //so add it to the result
+        if (currMaxHeight != prevMaxHeight) {
+            res.add(Arrays.asList(buildingPoint[0], currMaxHeight));
+            prevMaxHeight = currMaxHeight;
+        }
+    }
+    return res;
+}
