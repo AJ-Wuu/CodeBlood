9ff2a583ea5567c89a4f8315316e4beafa360419
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-24 02:15:07 +0800

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -29,3 +29,28 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //#239 - Sliding Window Maximum
+public static int[] maxSlidingWindow(int[] nums, int k) {
+    if (nums == null || k <= 0) {
+        return new int[0];
+    }
+		int n = nums.length;
+		int[] r = new int[n-k+1];
+		int index = 0; //store index
+		Deque<Integer> q = new ArrayDeque<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+        //remove numbers out of range k
+        while (!q.isEmpty() && q.peek() < i - k + 1) {
+            q.poll();
+        }
+        //remove smaller numbers in k range as they are useless
+        while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+            q.pollLast();
+        }
+        //q contains index... r contains content
+        q.offer(i);
+        if (i >= k - 1) {
+            r[index++] = nums[q.peek()];
+        }
+    }
+    return r;
+}
