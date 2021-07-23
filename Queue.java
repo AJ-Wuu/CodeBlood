61fe1ae843310f092f06d69cc9317b0750d680a1
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-24 02:23:03 +0800

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -30,9 +30,6 @@
 
 //#239 - Sliding Window Maximum
 public static int[] maxSlidingWindow(int[] nums, int k) {
-    if (nums == null || k <= 0) {
-        return new int[0];
-    }
     int n = nums.length;
     int[] result = new int[n-k+1];
     int index = 0; //store index
