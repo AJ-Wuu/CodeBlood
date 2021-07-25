77912b2d9eea28c291e43f17879515844d5a9f81
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-25 19:58:54 +0800

    Update Array.java

--- Array.java
+++ Array.java
@@ -264,3 +264,27 @@ else {
         //-> Check (and record as a flag) if the first row has 0.
         //-> If (matrix[i][j] == 0), matrix[i][0] = matrix[0][j] = 0. Then if (matrix[i][0] == 0 || matrix[0][j] == 0), matrix[i][j] = 0.
         //-> If the first row has 0, Arrays.fill(matrix[0], 0).
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#264 - Ugly Number II
+public static int nthUglyNumber(int n) {
+    int[] ugly = new int[n];
+    ugly[0] = 1;
+    int index2 = 0, index3 = 0, index5 = 0;
+    int factor2 = 2, factor3 = 3, factor5 = 5;
+    for (int i=1; i<n; i++) {
+        int min = Math.min(Math.min(factor2,factor3),factor5);
+        ugly[i] = min;
+        if (factor2 == min) {
+            factor2 = 2*ugly[++index2];
+        }
+        if (factor3 == min) {
+            factor3 = 3*ugly[++index3];
+        }
+        if (factor5 == min) {
+            factor5 = 5*ugly[++index5];
+        }
+    }
+    return ugly[n-1];
+}
