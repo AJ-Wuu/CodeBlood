24b4c7cc4415e808d387cd0cb99021665f19d2b2
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-28 23:21:35 +0800

    Update List.java

--- List.java
+++ List.java
@@ -1,1 +1,3 @@
 //As seen in performance comparison, ArrayList is better for storing and accessing data. LinkedList is better for manipulating data.
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
