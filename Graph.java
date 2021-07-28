fa3de67583b62f4a58d74922a55a0151849ac96a
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-28 16:11:20 +0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,4 +1,10 @@
 /**
- * Shallow Copy: If the field value is a reference to an object (e.g., a memory address) it copies the reference, hence referring to the same object as A does, and if the field value is a primitive type it copies the value of the primitive type. In languages without primitive types (where everything is an object), all fields of the copy B are references to the same objects as the fields of original A. The referenced objects are thus shared, so if one of these objects is modified (from A or B), the change is visible in the other. Shallow copies are simple and typically cheap, as they can be usually implemented by simply copying the bits exactly.
- * Deep Copy: fields are dereferenced: rather than references to objects being copied, new copy objects are created for any referenced objects, and references to these placed in B. The result is different from the result a shallow copy gives in that the objects referenced by the copy B are distinct from those referenced by A, and independent. Deep copies are more expensive, due to needing to create additional objects, and can be substantially more complicated, due to references possibly forming a complicated graph.
+ * Shallow Copy: If the field value is a reference to an object (e.g., a memory address) it copies the reference.
+ *               The referenced objects are thus shared, so if one of these objects is modified (from A or B), the change is visible in the other.
+ *               Shallow copies are simple and typically cheap, as they can be usually implemented by simply copying the bits exactly.
+ * Deep Copy: Fields are dereferenced.
+ *            Rather than references to objects being copied, new copy objects are created for any referenced objects, and references to these placed in B.
+ *            The objects referenced by the copy B are distinct from those referenced by A, and independent.
+ *            Deep copies are more expensive, due to needing to create additional objects, 
+ *                and can be substantially more complicated, due to references possibly forming a complicated graph.
  */
