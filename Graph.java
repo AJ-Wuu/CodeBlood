3eee0cb3b5f9404575dbba6205c5bb93bd535df9
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-01 03:14:42 +0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -253,3 +253,10 @@ public int dfs(int[][] matrix, int[][] cache, int x, int y, int curPoint) {
     cache[x][y] = max;
     return max;
 }
+
+
+//In Eulerian paths, there must exist a start node(which is JFK in this problem) and a end node.
+End node can be start node or another node.
+end node is start node iff all nodes has even degree.
+end node is another node iff there is another odd degree node and start node has an odd degree.
+So, the algorithm is to find the end node first and delete the path to this node(backtrack), meanwhile using PriorityQueue to guarantee lexical order.
