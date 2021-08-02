999f7b4fb6fd0596d44c99f0fec906c8c4286d5f
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-03 03:52:07 +0800

    Update List.java

--- List.java
+++ List.java
@@ -11,11 +11,11 @@ public static ListNode reverseKGroup(ListNode head, int k) {
     ListNode init = new ListNode(0); //init is the node before head
     init.next = head;
     for (ListNode prev=init, tail=head; n>=k; n-=k) {
-        for (int i=1; i<k; i++) { //21345 -> 32145 -> 43215
-            ListNode next = tail.next.next;
-            tail.next.next = prev.next;
-            prev.next = tail.next;
-            tail.next = next;
+        for (int i=1; i<k; i++) { //Eg. 12345 with k = 4
+            ListNode next = tail.next.next; //3 -- 4
+            tail.next.next = prev.next; //12121 -- 
+            prev.next = tail.next; //21212 -- 
+            tail.next = next; //21345 -- 
         }
         prev = tail;
         tail = tail.next;
