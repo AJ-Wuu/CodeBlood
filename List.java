5f1c0c13aa3558d7973434f91c2be5f5fb27a1cb
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-05 04:45:08 +0800

    Update List.java

--- List.java
+++ List.java
@@ -53,3 +53,33 @@ public static ListNode rotateRight(ListNode head, int k) {
     slow.next = null;
     return newHead;
 }
+
+//Sentinel Head + Predecessor
+public static ListNode deleteDuplicates(ListNode head) {
+        // sentinel
+        ListNode sentinel = new ListNode(0, head);
+
+        // predecessor = the last node 
+        // before the sublist of duplicates
+        ListNode pred = sentinel;
+        
+        while (head != null) {
+            // if it's a beginning of duplicates sublist 
+            // skip all duplicates
+            if (head.next != null && head.val == head.next.val) {
+                // move till the end of duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                // skip all duplicates
+                pred.next = head.next;     
+            // otherwise, move predecessor
+            } else {
+                pred = pred.next;    
+            }
+                
+            // move forward
+            head = head.next;    
+        }  
+        return sentinel.next;
+    }
