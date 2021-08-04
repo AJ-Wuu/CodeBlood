e6b490bbbcb32d9b40bf87edfdd7ed1c85481f92
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-05 04:48:12 +0800

    Update List.java

--- List.java
+++ List.java
@@ -54,32 +54,33 @@ public static ListNode rotateRight(ListNode head, int k) {
     return newHead;
 }
 
-//Sentinel Head + Predecessor
-public static ListNode deleteDuplicates(ListNode head) {
-        // sentinel
-        ListNode sentinel = new ListNode(0, head);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-        // predecessor = the last node 
-        // before the sublist of duplicates
-        ListNode pred = sentinel;
+//#82 - Remove Duplicates from Sorted List II
+//Method: Sentinel Head + Predecessor
+public static ListNode deleteDuplicates(ListNode head) {
+    //sentinel
+    ListNode sentinel = new ListNode(0, head);
+    //predecessor = the last node before the sublist of duplicates
+    ListNode pred = sentinel;
         
-        while (head != null) {
-            // if it's a beginning of duplicates sublist 
-            // skip all duplicates
-            if (head.next != null && head.val == head.next.val) {
-                // move till the end of duplicates sublist
-                while (head.next != null && head.val == head.next.val) {
-                    head = head.next;    
-                }
-                // skip all duplicates
-                pred.next = head.next;     
-            // otherwise, move predecessor
-            } else {
-                pred = pred.next;    
+    while (head != null) {
+    //if it's a beginning of duplicates sublist, skip all duplicates
+        if (head.next != null && head.val == head.next.val) {
+            //move till the end of duplicates sublist
+            while (head.next != null && head.val == head.next.val) {
+                head = head.next;    
             }
+            //skip all duplicates
+            pred.next = head.next;     
+            //otherwise, move predecessor
+        }
+        else {
+            pred = pred.next;    
+        }
                 
-            // move forward
-            head = head.next;    
-        }  
-        return sentinel.next;
-    }
+        //move forward
+        head = head.next;    
+    }  
+    return sentinel.next;
+}
