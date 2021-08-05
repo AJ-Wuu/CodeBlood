3cb535c8a87a9a6805f0f4fdfcad86efca95bea8
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-06 05:50:03 +0800

    Update DynamicProgramming.java

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -63,16 +63,16 @@ class Solution {
 
 //Time 86%, Space 81%
 //Idea: 1. If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
-2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
-3, If p.charAt(j) == '*': 
-   here are two sub conditions:
-               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
-               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
-                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
-                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
-                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+//2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+//3, If p.charAt(j) == '*': 
+//   here are two sub conditions:
+//               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+//               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+//                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
+//                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+//                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
 public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
+    if (s == null || p == null) {
         return false;
     }
     boolean[][] dp = new boolean[s.length()+1][p.length()+1];
@@ -93,11 +93,12 @@ public boolean isMatch(String s, String p) {
             if (p.charAt(j) == '*') {
                 if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
                     dp[i+1][j+1] = dp[i+1][j-1];
-                } else {
+                }
+                else {
                     dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
                 }
             }
         }
     }
     return dp[s.length()][p.length()];
-    }
+}
