cf6a31973a0a7dd8ac046b7592adf2f4c831eefd
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-05 22:23:26 +0800

    Update and rename Recursive.java to Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,9 @@
+/**
+ * Advantages:
+ * Disadvantages: 
+ *      1. A recursive program has greater space requirements than an iterative program as each function call will remain in the stack until the base case is reached.
+ *      2. It also has greater time requirements because each time the function is called, the stack grows and the final answer is returned when the stack is popped completely.
+ */
 //#2 - Add Two Numbers
 //Do not add the numbers together in an int or a long, as it's easily get overflowed.
 //Store the adding result for every bit, and use an int as carry digit
