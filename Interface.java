5c3e8bc0263b0cadb7a50e15f39f94feec57812c
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-15 16:18:21 +0800

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,11 @@
+/**
+ * "Programming for interfaces than implementation"
+ * Interfaces allow us to:
+ *     1. implement polymorphic behaviour (of course, abstract classes can also be used to implement polymorphic behaviour)
+ *     2. develop very loosely coupled systems
+ *     3. mock for better unit testing
+ *     4. implement multiple class inheritance
+ *     5. implement Inverson of Control or Dependancy Injection
+ *     6. enable parallel application development
+ *     7. avoid a massive hierarchical type framework
+ */
