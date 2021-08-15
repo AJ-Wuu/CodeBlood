06676d1a5cf2cc30a383aabd171394aea489387b
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-15 13:36:14 +0800

    Update Enum.java

--- Enum.java
+++ Enum.java
@@ -1,4 +1,4 @@
 /**
  * Enums are preferably to constants as they can enforce type safety.
- * Enums are basically classes, and they can implement interfaces, have behaviour and so on.
+ * Enums are basically classes; they can implement interfaces, have behaviour and so on.
  */
