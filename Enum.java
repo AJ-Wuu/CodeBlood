d78ff5c181ef84f9242e2accf083d87abe7d7a21
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-15 13:40:17 +0800

    Update Enum.java

--- Enum.java
+++ Enum.java
@@ -2,3 +2,19 @@
  * Enums are preferably to constants as they can enforce type safety.
  * Enums are basically classes; they can implement interfaces, have behaviour and so on.
  */
+public enum CardColour {
+  RED, BLACK
+}
+
+public enum Suit {
+  SPADES(CardColour.BLACK),
+  CLUBS(CardColour.BLACK),
+  HEARTS(CardColour.RED),
+  DIAMONDS(CardColour.RED);
+
+  private final CardColour colour;
+
+  Suit(CardColour colour) { this.colour = colour; }
+
+  public CardColour getColour() { return colour; }
+}
