b180a5d4ac87d4d96be99f1067c67e84ec52b163
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-09-04 19:07:19 -0500

    Update String.java

--- String.java
+++ String.java
@@ -87,3 +87,6 @@ public int lengthOfLongestSubstringKDistinct(String s, int k) {
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#5 - Longest Palindromic Substring
+//  https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
