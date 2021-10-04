411fe158b8a0de44f68091aacfbbbe2fe9d97d27
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-03 23:38:12 -0500

    Create MergeSort.java

---
+++ Sort/MergeSort.java
@@ -0,0 +1,72 @@
+/**
+ * @author AJWuu
+ */
+
+package merge;
+
+public class MergeSort {
+
+	//Divide and Conquer
+	
+	public static void merge(int[] array, int left, int mid, int right) {
+		int size1 = mid - left + 1;
+		int size2 = right - mid;
+		int[] leftHalf = new int[size1];
+		int[] rightHalf = new int[size2];
+		
+		//copy data into two temporary arrays
+		for (int i=0; i<size1; i++) {
+			leftHalf[i] = array[i+left];
+		}
+		for (int i=0; i<size2; i++) {
+			rightHalf[i] = array[i+mid+1];
+		}
+		
+		//pointers
+		int i = 0, j = 0;
+		int k = left;
+		while (i < size1 && j < size2) {
+			if (leftHalf[i] < rightHalf[j]) {
+				array[k] = leftHalf[i];
+				i++;
+			}
+			else {
+				array[k] = rightHalf[j];
+				j++;
+			}
+			k++;
+		}
+		while (i < size1) {
+			array[k] = leftHalf[i];
+			i++;
+			k++;
+		}
+		while (j < size2) {
+			array[k] = rightHalf[j];
+			j++;
+			k++;
+		}
+	}
+	
+	public static void divide(int[] array, int left, int right) {
+		if (left < right) {
+			int mid = (left + right) / 2;
+			divide(array, left, mid);
+			divide(array, mid+1, right);
+			merge(array, left, mid, right);
+		}
+	}
+	
+	public static void print(int[] array) {
+		for (int i=0; i<array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {21, 12, 11, 13, 4, 5, 3, 6, 2, 7, 1, 9, 8, 14, 41, 23, 31};
+		divide(array, 0, array.length-1);
+		print(array);
+	}
+
+}
