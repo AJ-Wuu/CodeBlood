d04340154e397210e89e6b5f90e975945d4695e8
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-05 00:02:37 -0500

    Update CountingSort.java

--- Sort/CountingSort.java
+++ Sort/CountingSort.java
@@ -2,48 +2,53 @@
  * @author AJWuu
  */
 
-package countingSort;
+package counting;
 
 import java.util.Arrays;
 
 public class CountingSort {
+	
+	//Get the min, max and length of the integer array
+	//Form a new array with min:1:max
+	//Count the number of each place (i.e. min = 0, max = 9, we have three 2s and two 8s, so count[2] = 3 and count[8] = 2)
+	//Generate the output array accordingly (i.e. count[0] = 3, count[1] = 0, count[2] = 2, count[3] = 1 -> {0,0,0,2,2,3})
 
-	public static void countSort(int[] arr) {
-		int max = Arrays.stream(arr).max().getAsInt();
-		int min = Arrays.stream(arr).min().getAsInt();
+	public static void countingSort(int[] array) {
+		int max = Arrays.stream(array).max().getAsInt();
+		int min = Arrays.stream(array).min().getAsInt();
 		int range = max - min + 1;
 		int count[] = new int[range];
-		int output[] = new int[arr.length];
+		int output[] = new int[array.length];
 		
-		for (int i=0; i<arr.length; i++) {
-			count[arr[i] - min]++;
+		for (int i=0; i<array.length; i++) {
+			count[array[i] - min]++;
 		}
 
 		for (int i=1; i<count.length; i++) {
 			count[i] += count[i - 1];
 		}
 
-		for (int i=arr.length-1; i>=0; i--) {
-			output[count[arr[i] - min] - 1] = arr[i];
-			count[arr[i] - min]--;
+		for (int i=array.length-1; i>=0; i--) {
+			output[count[array[i] - min] - 1] = array[i];
+			count[array[i] - min]--;
 		}
 
-		for (int i=0; i<arr.length; i++) {
-			arr[i] = output[i];
+		for (int i=0; i<array.length; i++) {
+			array[i] = output[i];
 		}
 	}
 
-	public static void printArray(int[] arr) {
-		for (int i=0; i<arr.length; i++) {
-			System.out.print(arr[i] + " ");
+	public static void print(int[] array) {
+		for (int i=0; i<array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
 		System.out.println("");
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {-5, -10, 0, -3, 8, 5, -1, 10};
-		countSort(arr);
-		printArray(arr);
+		int[] array = {12,22,9,4,32,23,2,24,1,21,3,42};
+		countingSort(array);
+		print(array);
 	}
 	
 }
