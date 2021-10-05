ac992b1278f9cfa0bf07905cf7b7c6263b9c0ed0
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-04 23:11:01 -0500

    Update InsertionSort.java

--- Sort/InsertionSort.java
+++ Sort/InsertionSort.java
@@ -6,6 +6,9 @@ package insertion;
 
 public class InsertionSort {
 
+	//Split the array into two parts: sorted | unsorted (initially, the bar is between index 0 and 1)
+	//Search through the elements before the bar to find the correct place to insert the first unsorted element (u)
+	//Move the elements larger than u to one place right and insert u in its location
 	//Binary Insertion uses binary search to find the proper location to insert the selected item (improving the searching efficiency from O(n) to O(logn))
 	//Time: O(n^2)
 	//Space: O(1)
