5047f3c75b5cacf5e7dd521568bed2d9a12ff6ee
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-06 01:00:42 -0500

    Update TimSort.java

--- Sort/TimSort.java
+++ Sort/TimSort.java
@@ -24,17 +24,17 @@ public class TimSort {
 	static int MERGE = 8; //This should be larger in real-work situation
 	
 	//input_size <= MERGE
-    public static void binaryInsertionSort(int[] array, int left, int right) {
-    	for (int i=1; i<array.length; i++) {
-            int x = array[i];
-            int j = Math.abs(Arrays.binarySearch(array, 0, i, x) + 1);
-            System.arraycopy(array, j, array, j + 1, i - j);
-            array[j] = x;
-        }
-    }
+	public static void binaryInsertionSort(int[] array, int left, int right) {
+    		for (int i=1; i<array.length; i++) {
+			int x = array[i];
+			int j = Math.abs(Arrays.binarySearch(array, 0, i, x) + 1);
+			System.arraycopy(array, j, array, j + 1, i - j);
+			array[j] = x;
+		}
+	}
 
-    public static void merge(int[] array, int left, int mid, int right) { 
-    	int size1 = mid - left + 1;
+	public static void merge(int[] array, int left, int mid, int right) { 
+    		int size1 = mid - left + 1;
 		int size2 = right - mid;
 		int[] leftHalf = new int[size1];
 		int[] rightHalf = new int[size2];
@@ -68,27 +68,27 @@ public class TimSort {
 			j++;
 			k++;
 		}
-    }
-  
-    //Iteratively to sort the array[0...n-1]
-    //Similar to merge sort
-    public static void timSort(int[] array, int n) {
-        //Sort sub-arrays of size MERGE
-        for (int i=0; i<n; i+=MERGE) {
-            binaryInsertionSort(array, i, Math.min((i + MERGE - 1), (n - 1)));
-        }
+	}
 
-        //Start merging from size MERGE
-        for (int size=MERGE; size<n; size=2*size) {
-            //Pick starting point of the left sub-array ([left, left+size-1] & [left+size, left+2*size-1])
-        	//After every merge, increase left by 2*size
-            for (int left=0; left<n; left+=2*size) { 
-                int mid = left + size - 1;
-                int right = Math.min((left + 2 * size - 1), (n - 1)); 
-                merge(array, left, mid, right);
-            }
-        }
-    }
+	//Iteratively to sort the array[0...n-1]
+	//Similar to merge sort
+	public static void timSort(int[] array, int n) {
+		//Sort sub-arrays of size MERGE
+		for (int i=0; i<n; i+=MERGE) {
+			binaryInsertionSort(array, i, Math.min((i + MERGE - 1), (n - 1)));
+		}
+		
+		//Start merging from size MERGE
+		for (int size=MERGE; size<n; size=2*size) {
+			//Pick starting point of the left sub-array ([left, left+size-1] & [left+size, left+2*size-1])
+			//After every merge, increase left by 2*size
+			for (int left=0; left<n; left+=2*size) { 
+				int mid = left + size - 1;
+				int right = Math.min((left + 2 * size - 1), (n - 1)); 
+				merge(array, left, mid, right);
+			}
+		}
+	}
 
 	public static void print(int[] array) {
 		for (int i=0; i<array.length; i++) {
