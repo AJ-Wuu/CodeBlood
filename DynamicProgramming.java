ec6ab0a9fcb2a45e16a24e887120996f8cdf5120
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-10 23:09:02 -0500

    Update DynamicProgramming.java

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -317,3 +317,20 @@ public boolean jumpGame(int[] arr, int start, boolean[] visited) {
     //Do NOT use if-else in this case
 }
 //Optimization: instead of spending O(n) to store visited[], we could directly "flip" the checked number to negative arr[i] = -arr[i]
+
+//
+public boolean canReach(String s, int minJ, int maxJ) {
+    int n = s.length(), pre = 0;
+    boolean[] dp = new boolean[n];
+    dp[0] = true;
+    for (int i=1; i<n; i++) {
+        if (i>=minJ && dp[i-minJ]) {
+            pre++;
+	}
+        if (i>maxJ && dp[i-maxJ-1]) {
+            pre--;
+	}
+        dp[i] = (pre>0 && s.charAt(i)=='0');
+    }
+    return dp[n - 1];
+}
