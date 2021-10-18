f26864db483e1a997f22823afd5fc14ad61fad4b
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-17 20:48:27 -0500

    Create Basics.java

---
+++ Search/Basics.java
@@ -0,0 +1,5 @@
+/**
+ * Comparison | Notion       | Data Structure | Memory      | Optimality
+ * BFS        | Vertex-based | Queue          | Inefficient | Optimal for finding the shortest distance, not in cost
+ * DFS        | Edge-based   | Stack          | Efficient   | NA
+ */
