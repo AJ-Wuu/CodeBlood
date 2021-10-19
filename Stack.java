a1c61682bb21d9a02eb5ac9dc77e81f572a9b092
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-19 18:27:55 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,6 +1,22 @@
 //#856 - Score of Parentheses
 //Approach 1: Divide and Conquer
 //Approach 2: Stack
+public static int scoreOfParentheses(String S) {
+    Stack<Integer> stack = new Stack<Integer>();
+    stack.push(0); //base score
+    for (char c : S.toCharArray()) {
+        if (c == '(') {
+            stack.push(0);
+        }
+        else {
+            int v = stack.pop(); //the score of the current frame
+            int w = stack.pop();
+            stack.push(w + Math.max(2 * v, 1)); //base score is 0, so 2 * v = 0
+        }
+    }
+    return stack.pop();
+}
+
 //Approach 3: Count Cores
 //    Keep track of the balance of the string.
 //    For every ) that immediately follows a (, the answer is 1 << balance, as balance is the number of exterior set of parentheses that contains this core.
