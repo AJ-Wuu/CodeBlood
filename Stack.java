b73109fc6229d8617e704417f2bb59f18e3fc834
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-19 18:24:45 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -4,3 +4,18 @@
 //Approach 3: Count Cores
 //    Keep track of the balance of the string.
 //    For every ) that immediately follows a (, the answer is 1 << balance, as balance is the number of exterior set of parentheses that contains this core.
+public static int scoreOfParentheses(String S) {
+    int ans = 0, balance = 0;
+    for (int i = 0; i < S.length(); ++i) {
+        if (S.charAt(i) == '(') {
+            balance++;
+        }
+        else {
+            balance--;
+            if (S.charAt(i-1) == '(') {
+                ans += 1 << balance;
+            }
+        }
+    }
+    return ans;
+}
