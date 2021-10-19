698cee57985519c27f572eff86995c1baf4e4f5d
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-19 18:23:38 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,3 +1,6 @@
-/*
- * Keep track of the balance of the string, as defined in Approach #1. For every ) that immediately follows a (, the answer is 1 << balance, as balance is the number of exterior set of parentheses that contains this core.
- */
+//#856 - Score of Parentheses
+//Approach 1: Divide and Conquer
+//Approach 2: Stack
+//Approach 3: Count Cores
+//    Keep track of the balance of the string.
+//    For every ) that immediately follows a (, the answer is 1 << balance, as balance is the number of exterior set of parentheses that contains this core.
