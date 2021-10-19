5ee2e7618802b2384217177bbb98b102d435fa97
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-10-19 18:21:32 -0500

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,3 @@
+/*
+ * Keep track of the balance of the string, as defined in Approach #1. For every ) that immediately follows a (, the answer is 1 << balance, as balance is the number of exterior set of parentheses that contains this core.
+ */
