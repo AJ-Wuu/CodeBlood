a41a8e4e0b25aae14b3ecb878099f16201853278
JayW <84046974+AJ-Wuu@users.noreply.github.com>
2021-11-08 16:49:21 -0600

    Update AnonymousClass.java

--- AnonymousClass.java
+++ AnonymousClass.java
@@ -2,107 +2,71 @@
  * @author AJWuu
  */
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
+/*
+ * Reasons to use Anonymous Functions: 
+ * 1. The function is only ever called in one place
+ * 2. Declared in-line and in-line functions have advantages in that they can access variables in the parent scopes
+ * 3. More self-contained and readable
+ *
+ * In Java, a class can contain another class known as nested class.
+ * A nested class that doesn't have any name is known as an anonymous class.
+ * An anonymous class must be defined inside another class.
+ * Hence, it is also known as an anonymous inner class.
+ * 
+ * Syntax: 
+ * class outerClass {
+ *     // defining anonymous class
+ *     object1 = new Type(parameterList) { //Type can be a superclass that an anonymous class extends OR an interface that an anonymous class implements
+ *         // body of the anonymous class
+ *     };
+ * }
+ * 
+ * Reasons to use Anonymous Classes:
+ * 1. A particular class A is the only consumer for a Class B and no where else this B class can be used
+ * 2. Easy to maintain
+ * 3. Reduce verbosity (make the code more concise) 
+ * 4. Enable you to declare and instantiate a class at the same time
+ * 5. Objects are created whenever they are required (created to perform some specific tasks)
+ */
 
-public class CustomTextFieldSample extends Application {
-    
-    /*
-     * Reasons to use Anonymous Functions: 
-     * 1. The function is only ever called in one place
-     * 2. Declared inline and inline functions have advantages in that they can access variables in the parent scopes
-     * 3. More self-contained and readable
-     *
-     * In Java, a class can contain another class known as nested class.
-     * A nested class that doesn't have any name is known as an anonymous class.
-     * An anonymous class must be defined inside another class.
-     * Hence, it is also known as an anonymous inner class.
-     * 
-     * Syntax: 
-     * class outerClass {
-     *     // defining anonymous class
-     *     object1 = new Type(parameterList) { //Type can be a superclass that an anonymous class extends OR an interface that an anonymous class implements
-     *         // body of the anonymous class
-     *     };
-     * }
-     * 
-     * Reasons to use Anonymous Classes:
-     * 1. A particular class A is the only consumer for a Class B and no where else this B class can be used
-     * 2. Easy to maintain
-     * 3. Reduce verbosity (make the code more concise) 
-     * 4. Enable you to declare and instantiate a class at the same time
-     * 5. Objects are created whenever they are required (created to perform some specific tasks)
-     */
-    
-    final static Label label = new Label();
- 
-    @Override
-    public void start(Stage stage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 150);
-        stage.setScene(scene);
-        stage.setTitle("Text Field Sample");
- 
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(5);
-        grid.setHgap(5);
- 
-        scene.setRoot(grid);
-        final Label dollar = new Label("$");
-        GridPane.setConstraints(dollar, 0, 0);
-        grid.getChildren().add(dollar);
-        
-        final TextField sum = new TextField() {
-            @Override
-            public void replaceText(int start, int end, String text) {
-                if (!text.matches("[a-z, A-Z]")) {
-                    super.replaceText(start, end, text);                     
-                }
-                label.setText("Enter a numeric value");
-            }
- 
-            @Override
-            public void replaceSelection(String text) {
-                if (!text.matches("[a-z, A-Z]")) {
-                    super.replaceSelection(text);
-                }
-            }
-        };
- 
-        sum.setPromptText("Enter the total");
-        sum.setPrefColumnCount(10);
-        GridPane.setConstraints(sum, 1, 0);
-        grid.getChildren().add(sum);
-        
-        Button submit = new Button("Submit");
-        GridPane.setConstraints(submit, 2, 0);
-        grid.getChildren().add(submit);
-        
-        submit.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                label.setText(null);
-            }
-        });
-        
-        GridPane.setConstraints(label, 0, 1);
-        GridPane.setColumnSpan(label, 3);
-        grid.getChildren().add(label);
-        
-        scene.setRoot(grid);
-        stage.show();
-    }
- 
-    public static void main(String[] args) {
-        launch(args);
-    }
+class classPolygon {
+	public void display() {
+		System.out.println("Inside the Polygon class");
+	}
+}
+
+interface interfacePolygon {
+	public void display();
+}
+
+class AnonymousDemo {
+
+	public void createClassFromClass() {
+		//Anonymous Class extending Class Polygon
+		classPolygon pClass = new classPolygon() {
+			public void display() {
+				System.out.println("Inside an anonymous class.");
+			}
+		};
+		pClass.display();
+	}
+
+	public void createClassFromInterface() {
+		//Anonymous Class implementing Interface
+		interfacePolygon pInterface = new interfacePolygon() {
+			public void display() {
+				System.out.println("Inside an anonymous class.");
+			}
+		};
+		pInterface.display();
+	}
+}
+
+class Main {
+	public static void main(String[] args) {
+		AnonymousDemo A1 = new AnonymousDemo();
+		AnonymousDemo A2 = new AnonymousDemo();
+		A1.createClassFromClass();
+		A2.createClassFromInterface();
+	}
 }
