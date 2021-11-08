2e5b6907b2ea324b36c9c326ba71586357e380b2
JayW <84046974+AJ-Wuu@users.noreply.github.com>
2021-11-08 16:38:45 -0600

    Update AnonymousClass.java

--- AnonymousClass.java
+++ AnonymousClass.java
@@ -1,3 +1,7 @@
+/**
+ * @author AJWuu
+ */
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,6 +14,32 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class CustomTextFieldSample extends Application {
+    
+    /*
+     * Reasons to use Anonymous Functions: 
+     * 1. The function is only ever called in one place
+     * 2. Declared inline and inline functions have advantages in that they can access variables in the parent scopes
+     * 3. More self-contained and readable
+     *
+     * In Java, a class can contain another class known as nested class.
+     * A nested class that doesn't have any name is known as an anonymous class.
+     * An anonymous class must be defined inside another class.
+     * Hence, it is also known as an anonymous inner class.
+     * 
+     * Syntax: 
+     * class outerClass {
+     *     // defining anonymous class
+     *     object1 = new Type(parameterList) { //Type can be a superclass that an anonymous class extends OR an interface that an anonymous class implements
+     *         // body of the anonymous class
+     *     };
+     * }
+     * 
+     * Reasons to use Anonymous Classes:
+     * 1. A particular class A is the only consumer for a Class B and no where else this B class can be used
+     * 2. Easy to maintain
+     * 3. Reduce verbosity (make the code more concise) 
+     * 4. Enable you to declare and instantiate a class at the same time
+     */
     
     final static Label label = new Label();
  
