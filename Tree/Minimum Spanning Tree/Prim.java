32f51fa9f4b74e7fc317a9db7def1083dbe946c8
JayW <84046974+AJ-Wuu@users.noreply.github.com>
2021-11-12 18:39:52 -0600

    Create Prim.java

---
+++ Tree/Minimum Spanning Tree/Prim.java
@@ -0,0 +1,58 @@
+/**
+ * @author AJWuu
+ */
+
+package mst;
+
+public class Prim {
+
+	private static int V;
+	
+	private static int minKey(int key[], Boolean mstSet[]) {
+		int min = Integer.MAX_VALUE, min_index = -1;
+
+		for (int v = 0; v < V; v++) {
+			if (mstSet[v] == false && key[v] < min) {
+				min = key[v];
+				min_index = v;
+			}
+		}
+		
+		return min_index;
+	}
+	
+	private static void printPrim(int parent[], int graph[][]) {
+		System.out.println("Edge \tWeight");
+		for (int i = 1; i < V; i++)
+			System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
+	}
+
+	public static void primMST(int graph[][]) {
+		V = graph.length;
+		int parent[] = new int[V];
+		int key[] = new int[V];
+		Boolean mstSet[] = new Boolean[V];
+		for (int i = 0; i < V; i++) {
+			key[i] = Integer.MAX_VALUE;
+			mstSet[i] = false;
+		}
+
+		//Insert the first vertex in MST as root
+		key[0] = 0;
+		parent[0] = -1;
+		for (int count=0; count<V-1; count++) {
+			int u = minKey(key, mstSet);
+			mstSet[u] = true;
+
+			for (int v=0; v<V; v++) {
+				if (graph[u][v] != 0 && mstSet[v] == false && graph[u][v] < key[v]) {
+					parent[v] = u;
+					key[v] = graph[u][v];
+				}
+			}
+		}
+
+		printPrim(parent, graph);
+	}
+
+}
