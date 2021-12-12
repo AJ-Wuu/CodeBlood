265e7f13918e310b82c579be85ffa57f99d5de66
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2021-12-12 00:47:05 -0600

    Update Prim.java

--- Tree/Minimum Spanning Tree/Prim.java
+++ Tree/Minimum Spanning Tree/Prim.java
@@ -4,10 +4,10 @@
 
 package mst;
 
+import java.util.LinkedList;
+
 public class Prim {
-	
-	//THIS PRIM IS PROBABLY WRONG, NEED FURTHER CHECK!!!
-	
+
 	//Works for weighted, connected, undirected graphs
 	//Step1: Pick start vertex s
 	//Step2: Add s as root of T
@@ -15,7 +15,7 @@ public class Prim {
 
 	private static int V;
 	
-	private static int minKey(int key[], Boolean mstSet[]) {
+	private static int minKey(int[] key, boolean[] mstSet) {
 		int min = Integer.MAX_VALUE, min_index = -1;
 
 		for (int v = 0; v < V; v++) {
@@ -28,28 +28,32 @@ public class Prim {
 		return min_index;
 	}
 	
-	private static void printPrim(int parent[], int graph[][]) {
+	private static void printPrim(int[] parent, int[][] graph, LinkedList<Integer> order) {
 		System.out.println("Edge \tWeight");
-		for (int i = 1; i < V; i++)
-			System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
+		for (int i=1; i<V; i++) {
+			int temp = order.get(i);
+			System.out.println(parent[temp] + " - " + temp + "\t" + graph[temp][parent[temp]]);
+		}
 	}
 
-	public static void primMST(int graph[][]) {
+	public static void primMST(int src, int graph[][]) {
 		V = graph.length;
 		int parent[] = new int[V];
 		int key[] = new int[V];
-		Boolean mstSet[] = new Boolean[V];
+		boolean mstSet[] = new boolean[V];
+		LinkedList<Integer> order = new LinkedList<Integer>();
 		for (int i = 0; i < V; i++) {
 			key[i] = Integer.MAX_VALUE;
 			mstSet[i] = false;
 		}
 
 		//Insert the first vertex in MST as root
-		key[0] = 0;
-		parent[0] = -1;
-		for (int count=0; count<V-1; count++) {
+		key[src] = 0;
+		parent[src] = -1;
+		for (int count=0; count<V; count++) {
 			int u = minKey(key, mstSet);
 			mstSet[u] = true;
+			order.add(u);
 
 			for (int v=0; v<V; v++) {
 				if (graph[u][v] != 0 && mstSet[v] == false && graph[u][v] < key[v]) {
@@ -59,7 +63,7 @@ public class Prim {
 			}
 		}
 
-		printPrim(parent, graph);
+		printPrim(parent, graph, order);
 	}
 
 }
