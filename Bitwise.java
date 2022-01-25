347221fbc741d95b859a869c1a364ec16c8c40b4
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-01-25 17:29:55 -0600

    Create Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,17 @@
+//syntax: >>=, <<=
+
+//#201 - Bitwise AND of Numbers Range
+//Approach 1: Find the common part
+int coeff = 1;
+while (left != right) {
+    left >>= 1;
+    right >>= 1;
+    coeff <<= 1;
+}
+return (left * coeff);
+
+//Approach 2: Bitwise-AND of any two numbers will always produce a number less than or equal to the smaller number.
+while (n > m) {
+    n = n & (n-1);
+}
+return (m & n);
