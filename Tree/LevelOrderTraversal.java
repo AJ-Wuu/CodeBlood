bb617b83f58f3da88ad859544cbe80bfcb7b2c0f
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-02-22 09:20:24 -0600

    Update LevelOrderTraversal.java

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -24,52 +24,52 @@ public class LevelOrderTraversal {
 
 	public static List<List<Integer>> levelOrder(TreeNode root) {
 		List<List<Integer>> ans = new ArrayList<>();
-		computeZigZag(ans, root, 0);
+		computeLevelOrder(ans, root, 0);
 		return ans;
 	}
 
 	//#102 - Binary Tree Level Order Traversal
-	private static void computeLevelOrder(List<List<Integer>> ans, TreeNode curr, int level) {
+	private static void computeLevelOrder(List<List<Integer>> ans, TreeNode curr, int height) {
 		if (curr == null) {
 			return ;
 		}
-		if (ans.size() == level) {
+		if (ans.size() == height) {
 			ans.add(new ArrayList<Integer>());
 		}
-		ans.get(level).add(curr.val);
-		computeLevelOrder(ans, curr.left, level+1);
-		computeLevelOrder(ans, curr.right, level+1);
+		ans.get(height).add(curr.val);
+		computeLevelOrder(ans, curr.left, height+1);
+		computeLevelOrder(ans, curr.right, height+1);
 	}
 
 	//#107 - Binary Tree Level Order Traversal II
-	private static void computeLevelOrderBottom(List<List<Integer>> ans, TreeNode curr, int level) {
+	private static void computeLevelOrderBottom(List<List<Integer>> ans, TreeNode curr, int height) {
 		if (curr == null) {
 			return;
 		}
-		if (ans.size() == level) {
+		if (ans.size() == height) {
 			ans.add(0, new ArrayList<Integer>()); //void java.util.List.add(int index, List<Integer> element)
 		}
-		ans.get(ans.size()-level-1).add(curr.val);
-		computeLevelOrderBottom(ans, curr.left, level+1);
-		computeLevelOrderBottom(ans, curr.right, level+1);
+		ans.get(ans.size()-height-1).add(curr.val);
+		computeLevelOrderBottom(ans, curr.left, height+1);
+		computeLevelOrderBottom(ans, curr.right, height+1);
 	}
 
 	//#103 - Binary Tree Zigzag Level Order Traversal
-	private static void computeZigZag(List<List<Integer>> ans, TreeNode curr, int level) {
+	private static void computeZigZag(List<List<Integer>> ans, TreeNode curr, int height) {
 		if (curr == null) {
 			return;
 		}
-		if (ans.size() == level) {
+		if (ans.size() == height) {
 			ans.add(new ArrayList<Integer>());
 		}
-		if (level % 2 == 0) {
-			ans.get(level).add(curr.val);
+		if (height % 2 == 0) {
+			ans.get(height).add(curr.val);
 		}
 		else {
-			ans.get(level).add(0, curr.val);
+			ans.get(height).add(0, curr.val);
 		}
-		computeZigZag(ans, curr.left, level + 1);
-		computeZigZag(ans, curr.right, level + 1);
+		computeZigZag(ans, curr.left, height+1);
+		computeZigZag(ans, curr.right, height+1);
 	}
 
 	public static void main(String[] args) {
