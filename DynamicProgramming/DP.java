6ca10d2a092aef093c6a14f98226b1c2cd678e9a
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-08-31 13:51:35 -0500

    Update DP.java

--- DynamicProgramming/DP.java
+++ DynamicProgramming/DP.java
@@ -377,3 +377,18 @@ public boolean canReach(String s, int minJ, int maxJ) {
     }
     return dp[n - 1];
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#279 - Perfect Squares
+public int numSquares(int n) {
+    int[] dp = new int[n+1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0;
+    for (int i=1; i<n+1; i++) {
+        for (int j=1; j*j<=i; j++) { //"<="
+            dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+        }
+    }
+    return dp[n];
+}
