e448b8559e088109178931e631ae5485dec69762
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-09-08 18:43:47 -0500

    Create LinkedHashMap.java

---
+++ LinkedHashMap.java
@@ -0,0 +1,8 @@
+/*
+ * LinkedHashMap = HashMap + a Doubly-Linked List running through all of its entries
+ * Advantages:
+ *     compared to HashMap and Hashtable -- predictable iteration order
+ *                                          normally based on keys' insertion order, and not affected by re-insertion
+ *     compared to TreeMap -- a lower time complexity for operations like put(), containsKey()
+ *                            (TreeMap requires logarithmic time to add / locate elements in the tree)
+ */
