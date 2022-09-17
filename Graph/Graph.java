238d0e6eaa680d8142bbeb42869aea35208f9737
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-09-16 22:29:39 -0500

    Update Graph.java

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -351,3 +351,32 @@ private static String find(HashMap<String, String> parent, HashMap<String, Doubl
     ratio.put(s, ratio.get(s) * ratio.get(child));
     return grandchild;
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#684 - Redundant Connection
+public int[] findRedundantConnection(int[][] edges) {
+    int[] parent = new int[2001];
+    for (int i=0; i<parent.length; i++) {
+        parent[i] = i;
+    }
+        
+    for (int[] edge: edges) {
+        int f = edge[0], t = edge[1];
+        if (find(parent, f) == find(parent, t)) {
+            return edge;
+        }
+        else {
+            parent[find(parent, f)] = find(parent, t);
+        }
+    }
+        
+    return new int[2];
+}
+    
+private int find(int[] parent, int f) {
+    if (f != parent[f]) {
+        parent[f] = find(parent, parent[f]);
+    }
+    return parent[f];
+}
