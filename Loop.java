42c148bf3e81cbd464a5195a3f3354e31076e33d
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-11-11 08:37:22 -0600

    Update Loop.java

--- Loop.java
+++ Loop.java
@@ -1,3 +1,9 @@
 List<String> days = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", 
 				   "Friday", "Saturday", "Sunday"); //Set, Queue also work
 days.forEach(System.out::println);
+
+// series function call
+optimize opt[5] = { optimize1, optimize2, optimize3, optimize4, optimize5 };
+for (int i = 0; i < opt.length(); i++) {
+    opt[i](&v, dest);
+}
