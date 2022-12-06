60178694d315cd32a610d825b1895200287e1ce9
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-12-06 10:02:30 -0600

    Update String.java

--- String.java
+++ String.java
@@ -187,3 +187,34 @@ for (char c : s.toCharArray()) {
     ca[c - 'a']++;
 }
 String keyStr = String.valueOf(ca);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#43 - Multiply Strings
+//Key: num1[i] * num2[j] will be placed at indices [i + j, i + j + 1]
+//     1  2  3
+//  x     4  5
+//        1  5
+//     1  0
+//  0  5
+//     0  8
+// ...
+public String multiply(String num1, String num2) {
+    int m = num1.length(), n = num2.length();
+    int[] pos = new int[m + n];
+   
+    for(int i = m - 1; i >= 0; i--) {
+        for(int j = n - 1; j >= 0; j--) {
+            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
+            int p1 = i + j, p2 = i + j + 1;
+            int sum = mul + pos[p2];
+
+            pos[p1] += sum / 10;
+            pos[p2] = (sum) % 10;
+        }
+    }  
+    
+    StringBuilder sb = new StringBuilder();
+    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+    return sb.length() == 0 ? "0" : sb.toString();
+}
