7ac7f05876f1ff80d0390ddd9e245414e5946ce3
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2023-01-15 20:00:46 -0800

    Update Greedy.java

--- Greedy.java
+++ Greedy.java
@@ -90,3 +90,29 @@ public int[] nextGreaterElements(int[] nums) {
 //4. Check if the new integer overflow in 32-bit
 //   4.1 this could be done by checking each step (i.e. if (Integer.MAX_VALUE - result > currDigit) { return -1; }), both addition and multiplication
 //   4.2 or, we could use a long variable (resultInLong) to store it, and check if (resultInLong > Integer.MAX_VALUE) { return -1; } else { return (int)resultInLong; } 
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//#435 - Non-overlapping Intervals
+class myComparator implements Comparator<Interval> {
+    public int compare(Interval a, Interval b) {
+        return a.end - b.end;
+    }
+}
+
+public int eraseOverlapIntervals(Interval[] intervals) {
+    if (intervals.length == 0)  {
+        return 0;
+    }
+
+    Arrays.sort(intervals, new myComparator());
+    int end = intervals[0].end;
+    int count = 1;        
+    for (int i = 1; i < intervals.length; i++) {
+        if (intervals[i].start >= end) {
+            end = intervals[i].end;
+            count++;
+        }
+    }
+    return intervals.length - count;
+}
