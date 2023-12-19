1cfaa047c14491675ba8e0f37a9c61f0303c1195
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2023-12-18 20:52:11 -0800

    Update Array.java

--- Array.java
+++ Array.java
@@ -6,6 +6,23 @@
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+// #661 - Image Smoother
+for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+	int totalSum = 0;
+	int count = 0;
+	for (int x = Math.max(0, i-1); x < Math.min(rows, i+2); ++x) {    // verify if index goes out-of-bound for array
+	    for (int y = Math.max(0, j-1); y < Math.min(cols, j+2); ++y) {
+		totalSum += img[x][y];
+		count += 1;
+	    }
+	}
+	result[i][j] = totalSum / count;
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 //Modify Arrays.sort()
 //Note: compare() -> return 1, increasing; return 0, unchanged; return -1, decreasing
 //Lambda:
