799cb5d0b05f2d3ce351305db4b04256d528a418
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2024-05-07 12:11:09 -0700

    Update Array.java

--- Array.java
+++ Array.java
@@ -4,6 +4,21 @@
 //Circular array: moving forward from the last element -> the first element, and vice versa
 //                int nextIndex = (currentIndex + arr[currentIndex]) % arr.length;
 
+// Convert byte[] to Object
+// Note that the deserialization method is valid only if the corresponding serialization method is used
+public static byte[] serialize(Object obj) throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ObjectOutputStream os = new ObjectOutputStream(out);
+    os.writeObject(obj);
+    return out.toByteArray();
+}
+
+public static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+    ByteArrayInputStream in = new ByteArrayInputStream(data);
+    ObjectInputStream is = new ObjectInputStream(in);
+    return is.readObject();
+}
+
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // #661 - Image Smoother
