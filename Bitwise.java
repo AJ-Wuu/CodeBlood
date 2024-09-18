a1116b657d5b68b846960800b065f3aaed3a4193
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2024-09-17 19:01:41 -0700

    Update Bitwise.java

--- Bitwise.java
+++ Bitwise.java
@@ -1,33 +1,33 @@
-//Bit Operators: & (and), | (or), ~ (not), ^ (xor), <<< (shift left), >>> (shift right)
-//Remove last bit: x&(x-1)
-//Exact last bit: x^(x&(x-1))
-//Number of 1 bits:
+// Bit Operators: & (and), | (or), ~ (not), ^ (xor), <<< (shift left), >>> (shift right)
+// Remove last bit: x&(x-1)
+// Exact last bit: x^(x&(x-1))
+// Number of 1 bits:
 public int hammingWeight(int n) {
     while (n) {
         n = n & (n-1);
         count++;
     }
 }
-//Power of Four: (n & (n-1) == 0) && (n & 0x55555555 != 0) //(n & (n-1) == 0) gets rid of the odd numbers, (n & 0x55555555 != 0) gets rid of power of 2 but not 4
-//Sum of Two Integers:
+// Power of Four: (n & (n-1) == 0) && (n & 0x55555555 != 0)    // (n & (n-1) == 0) gets rid of the odd numbers, (n & 0x55555555 != 0) gets rid of power of 2 but not 4
+// Sum of Two Integers:
 public int getSum(int a, int b) {
     return (b == 0) ? a : ((a ^ b) & ((a & b) <<< 1));
-    //(a ^ b) adds the bit that contains one 1 and one 0 to be 1
-    //(a & b << 1) checks if the bit contains two 1, and then shift 1-bit left to carry up
+    // (a ^ b) adds the bit that contains one 1 and one 0 to be 1
+    // (a & b << 1) checks if the bit contains two 1, and then shift 1-bit left to carry up
 }
-//Missing Numbers:
+// Missing Numbers:
 public int missingNumber(int[] arr) {
     int temp = 0;
     for (int i=0; i<arr.length; i++) {
-        temp ^= (i ^ arr[i]); //if (i == arr[j]), then i ^ arr[j] = 0
+        temp ^= (i ^ arr[i]);    // if (i == arr[j]), then i ^ arr[j] = 0
     }
     return (temp ^ arr.length);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#201 - Bitwise AND of Numbers Range
-//Approach 1: Find the common part
+// #201 - Bitwise AND of Numbers Range
+// Approach 1: Find the common part
 int coeff = 1;
 while (left != right) {
     left >>= 1;
@@ -36,30 +36,30 @@ while (left != right) {
 }
 return (left * coeff);
 
-//Approach 2: Bitwise-AND of any two numbers will always produce a number less than or equal to the smaller number.
+// Approach 2: Bitwise-AND of any two numbers will always produce a number less than or equal to the smaller number.
 while (n > m) {
     n = n & (n-1);
 }
 return (m & n);
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//#260 - Single Number III
+// #260 - Single Number III
 public int[] singleNumber(int[] nums) {
-    //Pass 1: Get the XOR of the two numbers we need to find
+    // Pass 1: Get the XOR of the two numbers we need to find
     int diff = 0;
     for (int num : nums) {
         diff ^= num;
     }
     
-    diff &= -diff; // Get its last set bit
+    diff &= -diff;    // Get its last set bit
         
-    //Pass 2
+    // Pass 2
     int[] rets = {0, 0};
     for (int num : nums) {
-        if ((num & diff) == 0) { // the bit is not set
+        if ((num & diff) == 0) {    // the bit is not set
             rets[0] ^= num;
         }
-        else { // the bit is set
+        else {    // the bit is set
             rets[1] ^= num;
         }
     }
