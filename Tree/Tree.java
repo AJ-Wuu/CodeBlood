1b8eec9dc0abb8868f6ed1a79c67ec59413477fa
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2024-09-28 16:20:37 -0700

    Update Tree.java

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -14,3 +14,37 @@ public void flatten(TreeNode root) {
     root.left = null;
     prev = root;
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// #386 - Lexicographical Numbers
+// Virtual Main Node
+// ↓                                  ↘                                                               ↘
+// 1                                   2                            ...                                9
+// ↓             ↘                     ↓        ↘                                                      ↓        ↘
+// 10             11        ...        20        21        ...        ...        ...        ...        90        91        ...
+// ↓   ↘          ↓                    ↓         ↓                                                     ↓         ↓
+// 100  101  ...  110  ...             200  ...  210  ...                                              900  ...  910  ...
+// ↓
+// ...
+public List<Integer> lexicalOrder(int n) {
+    List<Integer> result = new ArrayList<>();
+    for (int i = 1; i < 10; i++) { // loop through the main layers, starting with 1
+        dfs(i, n, result);
+    }
+    return result;
+}
+
+private void dfs(int current, int n, List<Integer> result) {
+    if (current > n) {
+        return;
+    }
+
+    result.add(current);
+    for (int i = 0; i < 10; ++i) {
+        if (10 * current + i > n) {
+            return;
+        }
+        dfs(10 * current + i, n, result);
+    }
+}
