ba12ba80247dd66c3ea6bef6a348d3b73435b6c5
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2024-09-30 08:44:37 -0700

    Update Graph.java

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -30,9 +30,9 @@
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#133 - Clone Graph
-//DFS, using recursion and visited map
-//Concerning duplicate labels situations (two nodes with the same label)
+// #133 - Clone Graph
+// DFS, using recursion and visited map
+// Concerning duplicate labels situations (two nodes with the same label)
 public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
     return clone(node, new HashMap<UndirectedGraphNode, UndirectedGraphNode>());
 }
@@ -61,23 +61,23 @@ public UndirectedGraphNode clone(UndirectedGraphNode src, HashMap<UndirectedGrap
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//207 - Course Schedule
-//prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai
-//Approach 1: Topological Sort (worse efficiency) -> See #210 or CLRS/Chapter22-Elementary Graph Algorithms/Directed Graph/TopologicalSort.java for details
-//Approach 2: DFS (Time Limit Exceeded)
-//Approach 3: BFS (better efficiency)
+// 207 - Course Schedule
+// prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai
+// Approach 1: Topological Sort (worse efficiency) -> See #210 or CLRS/Chapter22-Elementary Graph Algorithms/Directed Graph/TopologicalSort.java for details
+// Approach 2: DFS (Time Limit Exceeded)
+// Approach 3: BFS (better efficiency)
 public static boolean canFinish(int numCourses, int[][] prerequisites) {
-    ArrayList[] graph = new ArrayList[numCourses]; //prerequisite-requisite relationship
-    int[] degree = new int[numCourses]; //courses that are prerequisites (Eg. degree[1] = 2 means that course 1 is the prerequisite of two other courses)
-    Queue<Integer> queue = new LinkedList<Integer>(); //courses that are not prerequisites to any other courses
-    int count = 0; //how many courses have ever been added to queue
+    ArrayList[] graph = new ArrayList[numCourses]; // prerequisite-requisite relationship
+    int[] degree = new int[numCourses]; // courses that are prerequisites (Eg. degree[1] = 2 means that course 1 is the prerequisite of two other courses)
+    Queue<Integer> queue = new LinkedList<Integer>(); // courses that are not prerequisites to any other courses
+    int count = 0; // how many courses have ever been added to queue
     
-    //Initialize
+    // Initialize
     for (int i=0; i<numCourses; i++) {
         graph[i] = new ArrayList<Integer>();
     }
     
-    //Assign Values
+    // Assign Values
     for (int i=0; i<prerequisites.length; i++) {
         degree[prerequisites[i][1]]++;
         graph[prerequisites[i][0]].add(prerequisites[i][1]);
@@ -89,8 +89,8 @@ public static boolean canFinish(int numCourses, int[][] prerequisites) {
         }
     }
     
-    //Check if there are cycles
-    //If cycle exists, then the two courses will never be added to queue, as: 1. courses in the queue could never point to them; OR 2. their degrees will never reach 0
+    // Check if there are cycles
+    // If cycle exists, then the two courses will never be added to queue, as: 1. courses in the queue could never point to them; OR 2. their degrees will never reach 0
     while (queue.size() != 0) {
         int course = (int)queue.poll();
         for (int i=0; i<graph[course].size(); i++) {
@@ -112,13 +112,13 @@ public static boolean canFinish(int numCourses, int[][] prerequisites) {
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#210 - Course Schedule II
-//Topological Sort Approach
+// #210 - Course Schedule II
+// Topological Sort Approach
 static boolean hasCycle(ArrayList<Integer>[] list, boolean[] visited, boolean[] restack, Queue<Integer> queue, int i){
-    if (restack[i]) { //deal with cycle, like [5,5] or [[1,2], [2,1]]
+    if (restack[i]) { // deal with cycle, like [5,5] or [[1,2], [2,1]]
         return true;
     }
-    if (visited[i]) { //stop recursion
+    if (visited[i]) { // stop recursion
         return false;
     }
         
@@ -166,13 +166,13 @@ public static int[] findOrder(int numCourses, int[][] prerequisites) {
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#310 - Minimum Height Trees
-//Key: 1. For a tree-alike graph, the number of centroids is no more than 2
-//     2. Center is always the middle node in the longest path(s) along the tree
-//Process: 1. Once we trim out the first layer of the leaf nodes (nodes that have only one connection), some of the non-leaf nodes would become leaf nodes.
-//         2. The trimming process continues until there are only two nodes left in the graph, which are the centroids that we are looking for.
+// #310 - Minimum Height Trees
+// Key: 1. For a tree-alike graph, the number of centroids is no more than 2
+//      2. Center is always the middle node in the longest path(s) along the tree
+// Process: 1. Once we trim out the first layer of the leaf nodes (nodes that have only one connection), some of the non-leaf nodes would become leaf nodes.
+//          2. The trimming process continues until there are only two nodes left in the graph, which are the centroids that we are looking for.
 public ArrayList<Integer> findMinHeightTrees(int n, int[][] edges) {
-    //Base cases
+    // Base cases
     if (n < 2) {
         ArrayList<Integer> centroids = new ArrayList<Integer>();
         for (int i=0; i<n; i++) {
@@ -181,7 +181,7 @@ public ArrayList<Integer> findMinHeightTrees(int n, int[][] edges) {
         return centroids;
     }
     
-    //Build the graph
+    // Build the graph
     int[] degree = new int[n];
     HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
     for (int i=0; i<n; i++) {
@@ -194,7 +194,7 @@ public ArrayList<Integer> findMinHeightTrees(int n, int[][] edges) {
         degree[edge[1]]++;
     }
     
-    //Trim the leaves until reaching the centroids
+    // Trim the leaves until reaching the centroids
     ArrayList<Integer> leaves = new ArrayList<>();
     for (int i=0; i<n; i++) {
         if (degree[i] <= 1) {
@@ -206,43 +206,43 @@ public ArrayList<Integer> findMinHeightTrees(int n, int[][] edges) {
     int c = leaves.size();
     while (c < n) {
         ArrayList<Integer> newLeaves = new ArrayList<Integer>();
-        for (int leaf : leaves) { //remove the current leaves along with the edges
+        for (int leaf : leaves) { // remove the current leaves along with the edges
             for (int neighbor : map.get(leaf)) {
-                degree[neighbor]--; //picking off the leaf, so decrease degree for each of its neighbors
+                degree[neighbor]--; // picking off the leaf, so decrease degree for each of its neighbors
                 if (degree[neighbor] == 1) {
                     newLeaves.add(neighbor);
                 }
             }
             degree[leaf] = 0;
         }
-        c += newLeaves.size(); //all nodes added to leaves (tracking using c since we need to replace leaves with newLeaves)
+        c += newLeaves.size(); // all nodes added to leaves (tracking using c since we need to replace leaves with newLeaves)
         leaves = newLeaves;
     }
     
-    //The remaining 1 (or 2) node(s) in the leaves would be the centroid(s) of the tree which considered as minimum height tree's root node
+    // The remaining 1 (or 2) node(s) in the leaves would be the centroid(s) of the tree which considered as minimum height tree's root node
     return leaves;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#329 - Longest Increasing Path in a Matrix
-//Key: 1. Use matrix[x][y] <= matrix[i][j] so we don't need a visited[m][n] array
-//     2. Cache the distance because it's highly possible to revisit a cell
+// #329 - Longest Increasing Path in a Matrix
+// Key: 1. Use matrix[x][y] <= matrix[i][j] so we don't need a visited[m][n] array
+//      2. Cache the distance because it's highly possible to revisit a cell
 final int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 public int longestIncreasingPath(int[][] matrix) {
-    //Base Case
+    // Base Case
     if (matrix.length == 0) {
         return 0;
     }
 
-    //Traverse all points in matrix, use every point as starting point to do dfs traversal
-    //DFS function returns max increasing path after comparing four max return distance from four directions
+    // Traverse all points in matrix, use every point as starting point to do dfs traversal
+    // DFS function returns max increasing path after comparing four max return distance from four directions
     int result = 0;
     int n = matrix.length, m = matrix[0].length;
-    int[][] cache = new int[n][m]; //represent the longest increasing path starts from point matrix[i][j]
+    int[][] cache = new int[n][m]; // represent the longest increasing path starts from point matrix[i][j]
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            int curLen = dfs(matrix, cache, i, j, matrix[i][j]); //do DFS for every cell
+            int curLen = dfs(matrix, cache, i, j, matrix[i][j]); // do DFS for every cell
             result = Math.max(result, curLen);
         }
     }
@@ -254,34 +254,34 @@ public int dfs(int[][] matrix, int[][] cache, int x, int y, int curPoint) {
         return cache[x][y];
     }
 
-    //Initialize max distance as 1, since the path includes starting point itself
+    // Initialize max distance as 1, since the path includes starting point itself
     int max = 1;
     for (int[] dir : dirs) {
         int dx = x + dir[0];
         int dy = y + dir[1];
         
-        //Compare every 4 direction and skip cells that are out of boundary or smaller
+        // Compare every 4 direction and skip cells that are out of boundary or smaller
         if (dx < 0 || dx > matrix.length - 1 || dy < 0 || dy > matrix[0].length - 1 || curPoint >= matrix[dx][dy]) {
-            //next point is invalid (out of bound or GE next point)
+            // next point is invalid (out of bound or GE next point)
             continue;
         }
-        int curLen = 1 + dfs(matrix, cache, dx, dy, matrix[dx][dy]); //next point is valid
-        max = Math.max(max, curLen); //matrix max (getting from every cell's max)
+        int curLen = 1 + dfs(matrix, cache, dx, dy, matrix[dx][dy]); // next point is valid
+        max = Math.max(max, curLen); // matrix max (getting from every cell's max)
     }
     
-    //Update max increasing path value starting from current point in cache
+    // Update max increasing path value starting from current point in cache
     cache[x][y] = max;
     return max;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#332 - Reconstruct Itinerary
-//AKA the Eulerian Path
-//Keys: 1. there must exist a start node(which is JFK in this problem) and a end node
-//      2. end node can be the start node or another node
-//          2.1. end node is start node iff all nodes has even degree
-//          2.2. end node is another node iff there is another odd degree node and start node has an odd degree
+// #332 - Reconstruct Itinerary
+// AKA the Eulerian Path
+// Keys: 1. there must exist a start node(which is JFK in this problem) and a end node
+//       2. end node can be the start node or another node
+//           2.1. end node is start node iff all nodes has even degree
+//           2.2. end node is another node iff there is another odd degree node and start node has an odd degree
 public static List<String> findItinerary(List<List<String>> tickets) {
     HashMap<String, PriorityQueue<String>> targets = new HashMap<String, PriorityQueue<String>>();
     for (List<String> ticket : tickets) {
@@ -289,7 +289,7 @@ public static List<String> findItinerary(List<List<String>> tickets) {
     }
     List<String> route = new LinkedList<String>();
     Stack<String> stack = new Stack<String>();
-    stack.push("JFK"); //add the start node
+    stack.push("JFK"); // add the start node
     while (!stack.empty()) {
         while (targets.containsKey(stack.peek()) && !targets.get(stack.peek()).isEmpty()) {
             stack.push(targets.get(stack.peek()).poll());
@@ -301,14 +301,14 @@ public static List<String> findItinerary(List<List<String>> tickets) {
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#399 - Evaluate Division
-//Approach 1: EulerianPath(start, end) + visited[] -> Eulerian Path see CLRS/Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
-//            -> Notion: Find every path from start to end (in queries) and get each ratio
-//Approach 2: Union-Find (faster than Approach 1)
-//            -> Notion: Get the ratio of every element in a path with the same parameter, eg. (a/b, b/c, a/d) -> (a/d, b/d, c/d, d/d)
+// #399 - Evaluate Division
+// Approach 1: EulerianPath(start, end) + visited[] -> Eulerian Path see CLRS/Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
+//             -> Notion: Find every path from start to end (in queries) and get each ratio
+// Approach 2: Union-Find (faster than Approach 1)
+//             -> Notion: Get the ratio of every element in a path with the same parameter, eg. (a/b, b/c, a/d) -> (a/d, b/d, c/d, d/d)
 public static double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-    HashMap<String, String> parent = new HashMap<String, String>();  //<node, parent of the node>
-    HashMap<String, Double> ratio = new HashMap<String, Double>();   //<node, node / parent>
+    HashMap<String, String> parent = new HashMap<String, String>();  // <node, parent of the node>
+    HashMap<String, Double> ratio = new HashMap<String, Double>();   // <node, node / parent>
     for (int i=0; i<equations.size(); i++) {
         union(parent, ratio, equations.get(i).get(0), equations.get(i).get(1), values[i]);
     }
@@ -354,7 +354,7 @@ private static String find(HashMap<String, String> parent, HashMap<String, Doubl
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#684 - Redundant Connection
+// #684 - Redundant Connection
 public int[] findRedundantConnection(int[][] edges) {
     int[] parent = new int[2001];
     for (int i=0; i<parent.length; i++) {
@@ -383,10 +383,10 @@ private int find(int[] parent, int f) {
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#2101 - Detonate the Maximum Bombs
-//Key: Generally, bomb A can detonate bomb B, but not the other way round -> this implies a directed edge from A to B -> BFS or DFS to find all counts -> get the maximum
+// #2101 - Detonate the Maximum Bombs
+// Key: Generally, bomb A can detonate bomb B, but not the other way round -> this implies a directed edge from A to B -> BFS or DFS to find all counts -> get the maximum
 private boolean inRange(int[] a, int[] b) {
-    long dx = a[0] - b[0], dy = a[1] - b[1], r = a[2]; //watch out for integer overflow
+    long dx = a[0] - b[0], dy = a[1] - b[1], r = a[2]; // watch out for integer overflow
     return (dx * dx + dy * dy <= r * r);
 }
 
@@ -395,10 +395,10 @@ private int dfs(int idx, boolean[] visited, int[][] bombs) {
     visited[idx] = true;
     for (int i = 0; i < bombs.length; i++) {
         if (!visited[i] && inRange(bombs[idx], bombs[i])) {
-            count += dfs(i, visited, bombs); //add the possible detonation from bomb B
+            count += dfs(i, visited, bombs); // add the possible detonation from bomb B
 	}
     }
-    return (count + 1); //include itself
+    return (count + 1); // include itself
 }
 
 public int maximumDetonation(int[][] bombs) {
